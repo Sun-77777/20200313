11cd5369a43f618bab03675ee2bf0cadb21dc122
Sun-77777 <3198884722@qq.com>
2020-01-26 22:09:00 +0800

    用函数写二分查找的代码

---
+++ test_1_26_(2)/test_1_26_(2)/test.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int binary_search(int arr[],int k,int left,int right)
+{
+	while (left <= right)
+	{
+		int mid = left + (right - left) / 2;
+		if (arr[mid] < k)
+		{
+			left = mid + 1;
+		}
+		else if (arr[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else
+			return mid;
+	}
+	return -1;
+}
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9 };
+	int k = 7;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int ret = binary_search(arr, k, 0, sz - 1);
+	if (ret == -1)
+	{
+		printf("没找到\n");
+	}
+	else
+	{
+		printf("找到了：%d", ret);
+	}
+	return 0;
+}
