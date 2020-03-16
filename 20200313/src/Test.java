30ffea669e93283f5bb391bf337b3daeca64e634
Sun-77777 <3198884722@qq.com>
2020-03-16 23:33:16 +0800

    数组前面偶数后面奇数

--- 20200313/src/Test.java
+++ 20200313/src/Test.java
@@ -1,7 +1,25 @@
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
-        int[] array = {10,9,8,2,3,4,5};
-        bubble(array);
-        System.out.println(Arrays.toString(array));a);
+        int[] array = {1,9,8,2,3,4,5};
+        bubblesort(array);
+        System.out.println(Arrays.toString(array));
+    }
+    public static void bubblesort(int[] array) {
+        for(int i = 0; i < array.length - 1; i++) {
+            boolean flg = true;
+            for(int j = 0; j < array.length - 1 - i; j++) {
+                if(array[j] > array[j+1]) {
+                    int tmp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = tmp;
+                    flg = false;
+                }
+            }
+            if(flg) {
+                break;
+            }
+        }
     }
 }
