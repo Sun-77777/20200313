fa0b05e176bde6a2e206adebc9ff82e874862640
Sun-77777 <3198884722@qq.com>
2020-04-19 16:46:11 +0800

    树

--- 20200421/src/TestTree.java
+++ 20200421/src/TestTree.java
@@ -9,6 +9,13 @@ public class TestTree {
         public Node(char val) {
             this.val = val;
         }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "val=" + val +
+                    '}';
+        }
     }
 
     static Node build() {
@@ -39,9 +46,122 @@ public class TestTree {
         preOrder(root.left);
         preOrder(root.right);
     }
+    public static void inOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.val + " ");
+        inOrder(root.right);
+    }
+    public static void postOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.val + " ");
+    }
+
+    /*public static int treeSize = 0;
+    public static void size(Node root) {
+        if (root == null) {
+            return;
+        }
+        treeSize++;
+        size(root.left);
+        size(root.right);
+    }*/
+
+    public static int size(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int count = 0;
+        //借助递归思想把问题拆分
+        //整个树的节点个数= 根节点个数+左子树节点个数+右字数节点个数
+        return 1 + size(root.left) + size(root.right);
+    }
+    /*public static int leafSize = 0;
+    public static void leafSize(Node root) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            leafSize++;
+            return;
+        }
+        leafSize(root.left);
+        leafSize(root.right);
+    }*/
+
+    public static int leafSize(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right ==null) {
+            return 1;
+        }
+        //叶子节点个数 = 左子树叶子的节点个数 + 右子树叶子的节点个数
+        return leafSize(root.left) + leafSize(root.right);
+    }
+
+    public static int kLevelSize(Node root,int k) {
+        if (root == null || k < 1) {
+            return 0;
+        }
+        if (k == 1) {
+            return 1;
+        }
+        //把问题拆分
+        //求树第k层节点个数 = 左子树的k-1 层节点个数 + 右子树的k-1层节点个数
+        return kLevelSize(root.left,k-1) + kLevelSize(root.right,k-1);
+
+    }
+    /*public static Node result = null;
+    public static void find(Node root,char val) {
+        if (root == null) {
+            return;
+        }
+        if (root.val == val) {
+            result = root;
+            return;
+        }
+        find(root.left,val);
+        find(root.right,val);
+    }*/
+
+    public static Node find(Node root,int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        }
+        Node result = find(root.left,val);
+
+        if (result != null) {
+            return result;
+        }
+        return find(root.right,val);
+    }
 
     public static void main(String[] args) {
         Node root = build();
         preOrder(root);
+        System.out.println();
+        inOrder(root);
+        System.out.println();
+        postOrder(root);
+        size(root);
+        //System.out.println(treeSize);
+        int ret = leafSize(root);
+        System.out.println(ret);
+
+//        find(root,'A');
+//        System.out.println(result);
+
+        Node ret1 = find(root,'B');
+        System.out.println(ret1);
     }
 }
