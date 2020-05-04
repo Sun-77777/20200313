a410ee93e44b9bfdc1871c460d3d9045249618f1
Sun-77777 <3198884722@qq.com>
2020-05-05 00:00:09 +0800

    、

---
+++ 20200429/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int gen = 0;
+        int maxCycle = 1000;
+        Population p = new Population();
+        p.Initialize();// 初始化
+        while (gen <= maxCycle) {
+            p.Mutation();
+            p.CrossOver();
+            p.Selection();
+            gen++;
+            p.SaveBest();
+        }
+    }
+}
