1974774656170c4d985767b0946d593ce33afeab
Sun-77777 <3198884722@qq.com>
2020-05-08 21:45:45 +0800

    练习

--- 20200430/src/Sort.java
+++ 20200430/src/Sort.java
@@ -1,8 +1,58 @@
 import java.util.Arrays;
 
 public class Sort {
+    //归并排序
+    public static void mergeSort(int[] array) {
+        mergeSortHelper(array,0,array.length);
+    }
+
+    private static void mergeSortHelper(int[] array, int left, int right) {
+        if (right-left <= 1) {
+            return;
+        }
+        int mid = (left+right)/2;
+        mergeSortHelper(array,left,mid);
+        mergeSortHelper(array,mid,right);
+        merge(array,left,mid,right);
+    }
+
+    private static void merge(int[] array, int left, int mid, int right) {
+        int cur1 = left;
+        int cur2 = mid;
+
+        int[] output = new int[right-left];
+        int outputIndex = 0;
+
+        while (cur1 < mid && cur2 < right) {
+            //相等时 先出现的放在前面
+            if (array[cur1] <= array[cur2]) {
+                output[outputIndex] = array[cur1];
+                cur1++;
+                outputIndex++;
+            } else {
+                output[outputIndex] = array[cur2];
+                cur2++;
+                outputIndex++;
+            }
+        }
+        while (cur1 < mid) {
+            output[outputIndex] = array[cur1];
+            cur1++;
+            outputIndex++;
+        }
+        while (cur2 < right) {
+            output[outputIndex] = array[cur2];
+            cur2++;
+            outputIndex++;
+        }
+
+        for (int i = 0; i < right-left; i++) {
+            array[left+i] = output[i];
+        }
+    }
 
 
+    //堆排序
     public static void heapSort(int[] array) {
         //1.先建立对
         createHeap(array);
