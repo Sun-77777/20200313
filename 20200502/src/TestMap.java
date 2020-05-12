d01cf7978652834d60e8fd4829b7f83b8e49e56e
Sun-77777 <3198884722@qq.com>
2020-05-12 19:03:57 +0800

    练习

--- 20200502/src/TestMap.java
+++ 20200502/src/TestMap.java
@@ -1,7 +1,30 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TestMap {
+    public static int[] singleTwoNumber(int [] nums) {
+        int ret = 0;
+        for (int i : nums) {
+            ret ^= i;
+        }
+        int bit = 0;
+        for (; bit < 32; bit++) {
+            if ((ret & (1 << bit)) > 0) {
+                break;
+            }
+        }
+        int a = 0;
+        int b = 0;
+        for (int i : nums) {
+            if ((i & (1 << bit)) > 0) {
+                a ^= i;
+            } else {
+                b ^= i;
+            }
+        }
+        return new int[]{a,b};
+    }
     public int singleNumber(int[] nums) {
         Map<Integer,Integer> map = new HashMap<>();
         for (int i : nums) {
@@ -20,6 +43,9 @@ public class TestMap {
         return 0;
     }
     public static void main(String[] args) {
+        int[] nums = {1,1,2,2,3,5};
+        int[] ret = singleTwoNumber(nums);
+        System.out.println(Arrays.toString(ret));
 
     }
 }
